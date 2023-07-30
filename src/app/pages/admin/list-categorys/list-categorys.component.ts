e29b18ef656e9df09ded4796aeb436ff62012319
Diego Mejia <pcdiego17@gmail.com>
2023-07-30 10:10:55 -0600

    actualizado a angular 14

--- src/app/pages/admin/list-categorys/list-categorys.component.ts
+++ src/app/pages/admin/list-categorys/list-categorys.component.ts
@@ -1,4 +1,3 @@
-import { newArray } from '@angular/compiler/src/util';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { error } from 'console';
