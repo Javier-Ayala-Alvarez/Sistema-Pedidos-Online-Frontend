689bfd5056f4bc598da9c6552da4812d6840c93a
jayala <Ayala@example.com>
2023-08-07 21:49:56 -0600

    configuracion

--- src/app/services/helper.ts
+++ src/app/services/helper.ts
@@ -1,3 +1,4 @@
-let baserUrl = 'api-sistema-pedidos-linea-production.up.railway.app'
-export default baserUrl;
+// api-config.ts
+const baseUrl = 'https://api-sistema-pedidos-linea-production.up.railway.app';
 
+export default baseUrl;
