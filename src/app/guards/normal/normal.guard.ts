ba35c62be2366324a6d08a5a2ed6a7ed11c61b62
sishenkira2011 <nelson.karma2012@gmail.com>
2023-09-17 14:49:27 -0600

    funcionalidad de categoria finalizada

--- src/app/guards/normal/normal.guard.ts
+++ src/app/guards/normal/normal.guard.ts
@@ -14,7 +14,7 @@ export class NormalGuard implements CanActivate {
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-      if(this.loginService.isLoggedIn()&& this.loginService.getUserRole()=='NORMAL'){
+      if(this.loginService.isLoggedIn()&& this.loginService.getUserRole()=='CLIENTE'){
         return true;
       }
       this.router.navigate(['login']);
--- src/app/services/helper.ts
+++ src/app/services/helper.ts
@@ -1,6 +1,6 @@
 // api-config.ts
-const baseUrl = 'https://api-sistema-pedidos-linea-production.up.railway.app';
-//const baseUrl = 'http://localhost:8080';
+//const baseUrl = 'https://api-sistema-pedidos-linea-production.up.railway.app';
+let baseUrl = 'http://localhost:8080';
 
 
 
