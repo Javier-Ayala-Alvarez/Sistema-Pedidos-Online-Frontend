4bb8ab50d3ee6bb67d2fecb1e34a63f1163b48df
jayala <Ayala@example.com>
2023-09-20 20:05:04 -0600

    Actualizando la rama de produccion por cambios de Nelson

--- src/app/services/helper.ts
+++ src/app/services/helper.ts
@@ -1,10 +1,7 @@
 // api-config.ts
-//const baseUrl = 'https://api-sistema-pedidos-linea-production.up.railway.app';
-<<<<<<< HEAD
-const baseUrl = 'http://localhost:8080';
-=======
-let baseUrl = 'http://localhost:8080';
->>>>>>> experimental
+const baseUrl = 'https://api-sistema-pedidos-linea-production.up.railway.app';
+
+//const baseUrl = 'http://localhost:8080';
 
 
 
