6a8843e735fa2822c894236366e22fe59a754cf6
sishenkira2011 <nelson.karma2012@gmail.com>
2023-09-27 14:37:47 -0600

    se cambio archivo helper

--- src/app/services/helper.ts
+++ src/app/services/helper.ts
@@ -1,9 +1,6 @@
 // api-config.ts
 const baseUrl = 'https://api-sistema-pedidos-linea-production.up.railway.app';
-//let baseUrl = 'http://localhost:8080';
-
-
-
+//const baseUrl = 'http://localhost:8080';
 export default baseUrl;
 
 
