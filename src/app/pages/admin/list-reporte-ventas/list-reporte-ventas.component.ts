fcde6ef17459aa2b4f97a506bd0d39bdec7488e5
sishenkira2011 <nelson.karma2012@gmail.com>
2023-11-10 16:13:45 -0600

    se integro exportaciones a pdf

--- src/app/pages/admin/list-reporte-ventas/list-reporte-ventas.component.ts
+++ src/app/pages/admin/list-reporte-ventas/list-reporte-ventas.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { error } from 'console';
 import Swal from 'sweetalert2';
+//import pdfMake from 'pdfmake/build/pdfmake';
+//import pdfFonts from 'pdfmake/build/vfs_fonts';
+import * as pdfMake from "pdfmake/build/pdfmake";
+import * as pdfFonts from 'pdfmake/build/vfs_fonts';
+(<any>pdfMake).vfs = pdfFonts.pdfMake.vfs;
 import { RealizarVentaService } from 'src/app/services/realizarVenta/realizar-venta.service';
 
 @Component({
@@ -49,4 +54,39 @@ export class ListReporteVentasComponent implements OnInit {
     )
 
   }
+
+  crearPdf(){
+    this.realizarVentaService.listarReporteVentas().subscribe(
+      (dato:any)=>{
+        this.ventas=dato.body;
+      },
+      (error)=>{
+        console.log(error);
+        Swal.fire('Error !!','Error al cargar las ventas por sucursal','error');
+      }
+    )
+    console.log("aaaa",this.ventas.map((row: { id: any; nombre: any; direccion: any; ventas: any; })=>[row.id,row.nombre,row.direccion,row.ventas]));
+
+
+
+
+    const pdfDefinition:any={
+      content:[
+        {
+
+          table:{
+            widths: [ 'auto', 'auto', 'auto', '*' ],
+            body:[
+            ['ID','Nombre Sucursal','Direccion Sucursal','Total Ventas'],
+            ...this.ventas.map((row: { id: any; nombre: any; direccion: any; ventas: any; })=>[row.id,row.nombre,row.direccion,row.ventas])
+            ]
+          }
+        }
+      ]
+    }
+    const pdf=pdfMake.createPdf(pdfDefinition);
+    pdf.open();
+  }
+
+
 }
